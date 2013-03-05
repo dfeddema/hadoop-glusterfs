5a1e085173bafad85e6f9a82d0469958fb326aa4
jayunit100 <jayunit100@gmail.com>
2013-03-04 19:26:35 -0500

    script for deploying to vm

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#A Shell script for testing on VMs
+#RHS-1 VM-2 VM-3 /var/lib/hadoop
+echo "Edit the branch value in this script to test a different branch"
+export BRANCH=BZXXXX #Replace XXXX with the branch you intend to deploy.
+git pull
+git checkout $BRANCH
+mvn -Dmaven.test.skip=true package
+mv target/glusterfs-0.20.2-0.1.jar glustertest$BRANCH.jar
+echo "Now copying jar to VMs"
+#Edit the below lines if necessary to work on BAGL (i.e. change the server names).
+scp glustertest$BRANCH.jar server-1:/usr/lib/hadoop/lib/a0glusterfs.jar
+scp glustertest$BRANCH.jar server-2:/usr/lib/hadoop/lib/a0glusterfs.jar
+scp glustertest$BRANCH.jar server-3:/usr/lib/hadoop/lib/a0glusterfs.jar
+echo "Done copying priority named jar to servers"
